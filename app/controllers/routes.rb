057e05b84480e62043bd48f97ab91b24eaf03567
Zappat0n <albarros1973@gmail.com>
2021-05-21 22:38:56 +0200

    Fix error in production database

--- app/controllers/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  resources :static_pages, only: [:index]
-  root 'static_pages#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
